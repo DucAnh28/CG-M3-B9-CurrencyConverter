3c8f521ce3ee3999dfbb8a78c3f38cde1d5a54e9
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-08-17 15:50:25 +0700

    update

---
+++ src/main/java/com/example/currencyconverter/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.example.currencyconverter;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/convert")
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        float rate = Float.parseFloat(request.getParameter("rate"));
+        float usd = Float.parseFloat(request.getParameter("usd"));
+        float vnd = rate * usd;
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Rate: " + rate + "</h1>");
+        writer.println("<h1>USD: " + usd + "</h1>");
+        writer.println("<h1>VND: " + vnd + "</h1>");
+        writer.println("</html>");
+    }
+
+    public void destroy() {
+    }
+}
